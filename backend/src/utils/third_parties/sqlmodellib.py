fcde96bf6e0ece91c328f5fc7b45ef246441e65d
MUsoftware <musoftware@daum.net>
2025-10-05 18:54:06 +0900

    fix: Refactor get_json_schema to improve foreign key handling and read-only property checks

--- backend/src/utils/third_parties/sqlmodellib.py
+++ backend/src/utils/third_parties/sqlmodellib.py
@@ -3,7 +3,9 @@ from typing import TypedDict, cast
 from pydantic_core import PydanticUndefinedType
 from sqlmodel.main import SQLModel
 
-from .jsonschemalib import Schema, StringKeywords
+from .jsonschemalib import Schema
+
+READ_ONLY_COLUMNS = ("id", "created_at", "updated_at")
 
 
 def get_foreign_key_constraints(model: type[SQLModel]) -> dict[str, str]:
@@ -19,32 +21,61 @@ def get_foreign_key_constraints(model: type[SQLModel]) -> dict[str, str]:
 
 class SchemaInfo(TypedDict):
     schema: Schema
-    ui_schema: dict[str, dict[str, str | dict[str, str]]]
+    ui_schema: dict[str, dict[str, str | bool | dict[str, str]]]
 
 
 def get_json_schema(model: type[SQLModel]) -> SchemaInfo:
     foreign_keys = get_foreign_key_constraints(model)
     schema = cast(Schema, model.model_json_schema())
-    ui_schema: dict[str, dict[str, str | dict[str, str]]] = {}
+    ui_schema: dict[str, dict[str, str | bool | dict[str, str]]] = {}
 
     # for type checking
     assert isinstance(schema, dict)  # nosec B101
 
-    def handle_foreign_key_properties(property_name: str, property_info: StringKeywords) -> None:
-        if not (foreign_key := foreign_keys.get(property_name)):
-            return
+    def is_read_only_property(property_name: str, property_info: Schema) -> bool:
+        if not isinstance(property_info, dict):
+            return False
 
-        property_info |= {"minLength": 36, "maxLength": 36, "format": "uuid"}
+        return property_info.get("readOnly") or property_name in READ_ONLY_COLUMNS
+
+    def is_foreign_key_property(property_name: str, property_info: Schema, allow_recursive: bool = True) -> bool:
+        if not isinstance(property_info, dict):
+            return False
+
+        if "type" not in property_info:
+            if not allow_recursive:  # type: ignore[unreachable]
+                return False
+
+            # This might be a anyOf, oneOf, etc.
+            # If anyOf or oneOf field contains only two schemas and one of them is null, it is considered as nullable field.
+            # Check the other schema's type.
+            for keyword in ("anyOf", "oneOf"):
+                if keyword not in property_info:
+                    continue
+
+                for sub_property_info in property_info[keyword]:
+                    if is_foreign_key_property(property_name, sub_property_info, False):
+                        return True
+
+            return False
+
+        return property_info.get("type") == "string" and property_info.get("format") == "uuid" and property_name in foreign_keys
+
+    def handle_foreign_key_property(property_name: str, _: Schema) -> None:
         ui_schema[property_name] = {
-            "ui:widget": "ForeignKeyWidget",
-            "ui:options": {"resourceName": foreign_key.split(".")[0]},
+            "ui:field": "ForeignKeyField",
+            "ui:fieldReplacesAnyOrOneOf": True,
+            "ui:options": {"resourceName": foreign_keys[property_name].split(".")[0]},
         }
 
     for property_name, property_info in cast(dict[str, Schema], schema.get("properties", {})).items():
         if isinstance(property_info, bool):
             continue
 
-        if isinstance(property_info, dict) and property_info.get("type") == "string":
-            handle_foreign_key_properties(property_name, cast(StringKeywords, property_info))
+        if is_read_only_property(property_name, property_info):
+            continue
+
+        if is_foreign_key_property(property_name, property_info):
+            handle_foreign_key_property(property_name, property_info)
 
     return SchemaInfo(schema=schema, ui_schema=ui_schema)
